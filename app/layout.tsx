e5c2ff4a857d50a6a6ccd5bb7e036c81d8efd927
cgens67 <cgenshin587@gmail.com>
2026-04-19 03:01:33 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,18 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon:[
-      { url: '/icon-light-32x32.png', media: '(prefers-color-scheme: light)' },
-      { url: '/icon-dark-32x32.png', media: '(prefers-color-scheme: dark)' },
-      { url: '/icon.svg', type: 'image/svg+xml' }
+      {
+        url: '/icon-light-32x32.png',
+        media: '(prefers-color-scheme: light)',
+      },
+      {
+        url: '/icon-dark-32x32.png',
+        media: '(prefers-color-scheme: dark)',
+      },
+      {
+        url: '/icon.svg',
+        type: 'image/svg+xml',
+      },
     ],
     apple: '/apple-icon.png',
   },
@@ -29,8 +38,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${roboto.variable} font-sans antialiased overflow-hidden`}>
+    <html lang="en" className="bg-background">
+      <body className={`${roboto.variable} font-sans antialiased`}>
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
